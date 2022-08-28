1192fade918af43e48f6aa6c419fc5b5539e4dd8
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-28 22:08:01 +0100

    Installs Windows 7 ISO

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import getpass
 from pystyle import *
 import playsound 
+import requests
+
 
 playsound.playsound('moosiq.mp3', False)
 
@@ -30,34 +32,26 @@ print(Colorate.Horizontal(Colors.red_to_blue, f"{banner}", 1))
 print(Colorate.Horizontal(Colors.red_to_blue, f"{menu}", 1))
 print('')
 while True:
-    try:
-        choice = Write.Input("Select an option : ", Colors.red_to_blue, interval=0.0025)
+    choice = Write.Input("Select an option : ", Colors.red_to_blue, interval=0.0025)
 
-        if choice == 1:
-            print(Colorate.Color(Colors.green, f'Installing Windows XP', True))
-
-        elif choice == 2:
-            print(Colorate.Color(Colors.green, f'Installing Windows Vista', True))
-
-        elif choice == 3:
-            print(Colorate.Color(Colors.green, f'Installing Windows 7', True))
-
-        elif choice == 4:
-            print(Colorate.Color(Colors.green, f'Installing Windows 8', True))
-
-        elif choice == 5:
-            print(Colorate.Color(Colors.green, f'Installing Windows 10', True))
-
-        elif choice == 6:
-            print(Colorate.Color(Colors.green, f'Installing Windows 11', True))
-        elif choice == '':
-            print(Colorate.Color(Colors.red, f'Field Cant Be Empty', True))
-
+    if choice == 1:
+        print(Colorate.Color(Colors.green, f'Installing Windows XP', True))
+        url = 'https://bit.ly/3318e1R'
+        data = requests.get(url).content
+        with open('./Windows 7/7.iso', 'wb') as handler:
+            handler.write(data)
+            print(Colorate.Color(Colors.blue, f'Windows 7 ISO Installed', True))    
+    elif choice == 2:
+        print(Colorate.Color(Colors.green, f'Installing Windows Vista', True))  
+    elif choice == 3:
+        print(Colorate.Color(Colors.green, f'Installing Windows 7', True))  
+    elif choice == 4:
+        print(Colorate.Color(Colors.green, f'Installing Windows 8', True))  
+    elif choice == 5:
+        print(Colorate.Color(Colors.green, f'Installing Windows 10', True)) 
+    elif choice == 6:
+        print(Colorate.Color(Colors.green, f'Installing Windows 11', True)) 
+    elif choice == '':
+        print(Colorate.Color(Colors.red, f'Field Cant Be Empty', True))
 
 
-        elif choice != [1, 2, 3, 4, 5, 6]:
-            print(Colorate.Color(Colors.red, f'Invalid Option. READ!', True))
-
-    except KeyboardInterrupt:
-        print('')
-        print(Colorate.Color(Colors.red, f'FAILED TO INTERUPT.', True))
