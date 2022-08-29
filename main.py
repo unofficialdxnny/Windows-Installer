2ea6adde617e6cc6fb55cc75c73a679be50a1627
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-29 16:25:48 +0100

    Added sound play and wget for download animation

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from playsound import playsound
 import os
 import getpass
 from pystyle import *
-import playsound 
 import requests
 import pyautogui as pag
 from time import sleep
 from elevate import elevate
 import keyboard as kb
+import shutil
+import wget
+
 
 elevate()
 
-playsound.playsound('moosiq.mp3', False)
+playsound('moosiq.mp3', False)
+
 
 os.system('cls & title Windows Installer ~ unofficialdxnny')
 
@@ -36,6 +40,7 @@ opt = ['1', '2', '3', '4', '5', '6']
 
 while True:
     try:
+        os.system('cls')
         print(Colorate.Horizontal(Colors.red_to_blue, f"{banner}", 1))
         print(Colorate.Horizontal(Colors.red_to_blue, f"{menu}", 1))
         print('')
@@ -55,45 +60,55 @@ while True:
 
 
         elif choice == '3':
-            print(Colorate.Color(Colors.green, f'Installing Windows 7', True))  
-            url = 'https://bit.ly/3318e1R'
-            data = requests.get(url).content
-            with open('./Windows 7/7.iso', 'wb') as handler:
-                handler.write(data)
-                print(Colorate.Color(Colors.green, f'Windows 7 ISO Installed', True))  
-                print(Colorate.Color(Colors.green, f'Making Bootable USB...', True))
-                print(Colorate.Color(Colors.red_to_blue, f'Inserted USB? Press "Enter"', True))
+            print(Colorate.Color(Colors.green, f'Installing Windows 7', True))
+            ## playsound.playsound('./assets/sounds/startup7.mp3', True)
+  
+            ## url = 'https://bit.ly/3318e1R'
+            ## data = requests.get(url).content
+            ## with open('./Windows 7/7.iso', 'wb') as handler:
+            ##     handler.write(data)
+            
+            wget.download('https://bit.ly/3318e1R')
+            
+            print(Colorate.Color(Colors.green, f'Windows 7 ISO Installed', True))  
+            print(Colorate.Color(Colors.green, f'Making Bootable USB...', True))
+            print(Colorate.Color(Colors.red_to_blue, f'Inserted USB? Press "Enter"', True))
+            if kb.read_key('enter'):
+                os.system('diskpart')
+                os.system('list disk')
+                print(Colorate.Color(Colors.red_to_blue, f'Type "Disk #" of your disk', True))
+                disk = Write.Input("type 'Disk #' : ", Colors.red_to_blue, interval=0.0025)
+                os.system(disk)
+                print(Colorate.Color(Colors.red_to_blue, f'To make a pendrive bootable, there is a need to format it to clean the existing data. Make sure you made a backup.', True))
+                print(Colorate.Color(Colors.red_to_blue, f'Press "Enter" once done.', True))
                 if kb.read_key('enter'):
-                    os.system('diskpart')
-                    os.system('list disk')
-                    print(Colorate.Color(Colors.red_to_blue, f'Type "Disk #" of your disk', True))
-                    disk = Write.Input("type 'Disk #' : ", Colors.red_to_blue, interval=0.0025)
-
-                    os.system(disk)
-                    print(Colorate.Color(Colors.red_to_blue, f'To make a pendrive bootable, there is a need to format it to clean the existing data. Make sure you made a backup.', True))
-                    print(Colorate.Color(Colors.red_to_blue, f'Press "Enter" once done.', True))
-                    if kb.read_key('enter'):
-                        print(Colorate.Color(Colors.red_to_blue, f'Cleaning the disk.', True))
-                        os.system('clean')
-                        print(Colorate.Color(Colors.green, f'Successful...', True))
-                        print(Colorate.Color(Colors.red_to_blue, f'Making the disk bootable.', True))
-                        os.system('create partition primary')
-                        print(Colorate.Color(Colors.green, f'Successful...', True))
-                        print(Colorate.Color(Colors.red_to_blue, f'Selecting correct partition.', True))
-                        os.system('select partition 1')
-                        print(Colorate.Color(Colors.green, f'Successful...', True))
-                        print(Colorate.Color(Colors.red_to_blue, f'Formatting Drive', True))
-                        os.system('format=fs NTFS')
-                        print(Colorate.Color(Colors.green, f'Successful...', True))
-                        print(Colorate.Color(Colors.red_to_blue, f'Activating Bootable Drive', True))
-                        os.system('active')
-                        print(Colorate.Color(Colors.green, f'Successful...', True))
-                        print(Colorate.Color(Colors.red_to_blue, f'Exiting DISKPART', True))
-                        os.system('exit')
-                        print(Colorate.Color(Colors.green, f'Finished', True))
-                
-                sleep(1)
-                os.system('cls')
+                    print(Colorate.Color(Colors.red_to_blue, f'Cleaning the disk.', True))
+                    os.system('clean')
+                    print(Colorate.Color(Colors.green, f'Successful...', True))
+                    print(Colorate.Color(Colors.red_to_blue, f'Making the disk bootable.', True))
+                    os.system('create partition primary')
+                    print(Colorate.Color(Colors.green, f'Successful...', True))
+                    print(Colorate.Color(Colors.red_to_blue, f'Selecting correct partition.', True))
+                    os.system('select partition 1')
+                    print(Colorate.Color(Colors.green, f'Successful...', True))
+                    print(Colorate.Color(Colors.red_to_blue, f'Formatting Drive', True))
+                    os.system('format=fs NTFS')
+                    print(Colorate.Color(Colors.green, f'Successful...', True))
+                    print(Colorate.Color(Colors.red_to_blue, f'Activating Bootable Drive', True))
+                    os.system('active')
+                    print(Colorate.Color(Colors.green, f'Successful...', True))
+                    print(Colorate.Color(Colors.red_to_blue, f'Exiting DISKPART', True))
+                    os.system('exit')
+                    print(Colorate.Color(Colors.green, f'Finished', True))
+                    source_dir = Write.Input("path/to/source/directory :  ", Colors.red_to_blue, interval=0.0025)
+                    target_dir = Write.Input("path/to/destination/directory : ", Colors.red_to_blue, interval=0.0025)
+                    
+                    file_names = os.listdir(source_dir)
+                        
+                    for file_name in file_names:
+                        shutil.move(os.path.join(source_dir, file_name), target_dir)
+            sleep(1)
+            os.system('cls')
 
 
         elif choice == '4':
