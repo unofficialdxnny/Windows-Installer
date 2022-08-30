6b397bf4d61e6a72342ffe7df53358775680ff3f
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-08-30 22:33:37 +0100

    added windows vista

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ menu = '''
     [1] Windows XP        [4] Windows 8   
     [2] Windows Vista     [5] Windows 10   
     [3] Windows 7         [6] Windows 11
-              [7] Bootable USB
+          
 
 '''
 opt = ['1', '2', '3', '4', '5', '6']
@@ -127,7 +127,61 @@ while True:
 
 
         elif choice == '2':
-            print(Colorate.Color(Colors.green, f'Installing Windows Vista', True))  
+            ## print(Colorate.Color(Colors.green, f'Installing Windows Vista', True))  
+
+            console = Console()
+            tasks = [f"Loading Contents", "Retrieving URL", "Checking URL MetaData", "Loading URL", "Checking Internet Connection"]
+            for n in range(1, 11):
+                with console.status("[bold green]Working on tasks...") as status:
+                    while tasks:
+                        tim = random.randint(1,6)
+                        task = tasks.pop(0)
+                        sleep(tim)
+                        console.log(f"{task}")
+            
+            def connect(host='http://google.com'):#
+                try:
+                    urllib.request.urlopen(host) #Python 3.x
+                    return True
+                except:
+                    return False
+                    # test
+            
+            if connect():
+                done = False
+                #here is the animation
+                def animate():
+                    for c in itertools.cycle(['|', '/', '-', '\\']):
+                        if done:
+                            break
+                        sys.stdout.write(Fore.GREEN + '\rInstalling Windows Vista ' + c)
+                        sys.stdout.flush()
+                        time.sleep(1)
+                    ## sys.stdout.write(Fore.GREEN + '\rWindows XP Installed')
+
+                t = threading.Thread(target=animate)
+                t.start()
+                ## playsound.playsound('./assets/sounds/startup7.mp3', True)
+    
+                response = requests.get("https://bit.ly/WinVistaatsiVniW7002")
+
+                file = open("WindowsXP.iso", "wb")
+                file.write(response.content)
+                file.close()
+                print(Colorate.Color(Colors.green, f'Windows Vista ISO Installed', True))  
+
+                #long process here
+                time.sleep(10)
+                done = True
+
+                sleep(1)
+                os.system('cls')
+            else:
+                pag.keyDown('alt')
+                pag.keyDown('F4')
+                pag.keyUp('alt')
+                pag.keyUp('F4')
+
             sleep(1)
             os.system('cls')
 
